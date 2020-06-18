683be93523088ac9af7cfc023edc241cadeb4246
BrijeshBumrela <brijeshbumrela@gmail.com>
2020-06-18 23:13:20 +0530

    add disconnect socket and remove user helper function

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import Game from "./Model/Game";
 import User from "./Model/User";
 import { getAvailableRoom } from "./utils/getAvailableRoom";
 import { v4 as uuidv4 } from "uuid";
+import PlayService from "./services/playService";
 
 const app = express();
 const server = http.createServer(app);
@@ -18,21 +19,23 @@ io.on("connection", socket => {
     const user = new User(socket.id, socket.handshake.query.name);
     let game = getAvailableRoom(games);
     if (!game) {
-        game = new Game(uuidv4());
+        game = new Game(uuidv4(), new PlayService());
         games.push(game);
     }
     // Starting the game when the user limit is reached is in this function
     game.addUser(user);
+    console.log(game.room);
     socket.join(game.room);
 
     if (game.isRoomFilled()) {
-        game.users.forEach((user, index) => {
+        game.users.forEach(user => {
             if (game instanceof Game) {
                 io.to(user.id).emit("startgame", {
                     myInfo: {
                         card: user.cards[0],
                         position: game.getUserIndex(user)
                     },
+                    // Info about the user who is going to start the game
                     turnInfo: {
                         card: game.getUserWithTheTurn().cards[0],
                         position: game.getUserIndex(game.getUserWithTheTurn())
@@ -40,9 +43,20 @@ io.on("connection", socket => {
                 });
             }
         });
-        console.log(user);
     }
+
+    socket.on("disconnect", () => {
+        findUserAndRemoveFromGame(socket.id, games);
+    });
 });
+
+const findUserAndRemoveFromGame = (id: string, games: Game[]) => {
+    games.forEach(game => {
+        game.users.forEach((user, index) => {
+            if (user.id === id) game.users.splice(index, 1);
+        });
+    });
+};
 
 server.listen(8000);
 module.exports = app;
